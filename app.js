31e739746da3718c3a049d8431818ad28bb158d4
Manish kumar <113233807+Manishyolo@users.noreply.github.com>
2023-09-26 11:18:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,88 @@
-const wrapper = document.querySelector('.wrapper');
-let searchinput = wrapper.querySelector('input');
-infotext = wrapper.querySelector('.info-text');
-const synonym = wrapper.querySelector('.synonyms .list');
-let volumeicon = wrapper.querySelector('.word i')
-let removeicon = wrapper.querySelector('.search span')
-let audio;
-//data funtion//
-function data(result,word){
-    if(result.title){
-        infotext.innerHTML = `can't find the meaning of ${word}`;
+let wrapper = document.querySelector('.wrapper');
+let searchinput = wrapper.querySelector('.search input');
+let infoText = wrapper.querySelector('.info-text');
+let synonym = wrapper.querySelector('.synonyms .list');
+let closebtn = wrapper.querySelector('.search span');
+//data function//
+function data(word, result) {
+    if (result.title) {
+        infoText.innerHTML = `can't find the meaning of${word}`
     }
-    else{
+    else {
+        wrapper.classList.add('active');
         console.log(result)
-        //console.log(result[0].meanings[0].definitions[0].definition,word);
-       // infotext.innerHTML = result[0].meanings[0].definitions[0].definition;
-        let phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[0].text}/`
-       
-        wrapper.classList.add('active')
-        document.querySelector('.word p').innerText = result[0].word;
-        document.querySelector('.word span').innerText = phonetics;
-         document.querySelector('.meaning span').innerText = result[0].meanings[0].definitions[0].definition;
-         document.querySelector('.Example span').innerText = result[0].meanings[0].definitions[0].example;
-         audio = new Audio("https:" + result[0].phonetics[0].audio);
-         console.log(audio)
-         if(result[0].meanings[0].synonyms[0] == undefined){
+        let definition = result[0].meanings[0].definitions[0].definition;
+        let phonetics = ` ${result[0].meanings[0].partOfSpeech} ${result[0].phonetics[0].text}`;
+        let example = result[0].meanings[0].definitions[0].example;
+        let synonyms = result[0].meanings[0].synonyms
+        console.log(synonyms);
+        wrapper.querySelector('.word p').innerText = result[0].word;
+        wrapper.querySelector('.word span').innerText = phonetics
+        wrapper.querySelector('.meaning span').innerText = definition;
+
+        if (synonyms[0] == undefined || example == undefined) {
             synonym.parentElement.style.display = 'none'
-         }else{
+            wrapper.querySelector('.Example span').parentElement.parentElement.style.display = 'none'
+
+        }
+        else {
+            wrapper.querySelector('.Example span').parentElement.parentElement.style.display = 'block'
+            wrapper.querySelector('.Example span').innerText = example;
             synonym.innerHTML = "";
-            synonym.parentElement.style.display = 'block'
-            console.log(synonym.parentElement);
-            for(var i = 0; i < 5 ; i++){
-                let tag = `<span onclick=search('${result[0].meanings[0].synonyms[i]}')>${result[0].meanings[0].synonyms[i]}</span>`
-                synonym.insertAdjacentHTML('beforeend',tag);
-          }
-         }
-        
-      
+            synonym.parentElement.style.display = 'block';
+ 
+            for (i = 0; i < synonyms.length; i++) {
+                let tag = `<span onclick=search('${synonyms[i]}')>${synonyms[i]}</span>`
+             
+                synonym.insertAdjacentHTML('beforeend', tag);
+            }
+          
+        }
+
     }
+
+
+
+
 }
 
-function search(word){
-    searchinput.value = word;
-    fetchapi(word)
-}
 
-//fetch function //
+
+
+//fetching data from api and returning data to data function//
 function fetchapi(word) {
-    wrapper.classList.remove('active')
-    infotext.style.color = '#000'
-    infotext.innerHTML = `Searching the meaning ${word}`
+    wrapper.classList.remove('active');
+    infoText.style.color = '#636363';
+    infoText.innerText = `searching the meaning of ${word}`;
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url).then(response => response.json()).then(result => data(result,word)).catch(error => {
-        console.log(error)
+    fetch(url).then(response => response.json()).then(result => {
+
+        return data(word, result);
+    }).catch((error) => {
+        console.log(error);
     })
+
+
 }
 
 
 searchinput.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter' && e.target.value) {
+
+    if (e.key == 'Enter' && e.target.value) {
         fetchapi(e.target.value)
+
     }
 
-
-
 })
+// search function//
 
-volumeicon.addEventListener('click',()=>{
-     audio.play();
-})
+function search(word) {
+    searchinput.value = word;
+    fetchapi(word);
+}
 
-removeicon.addEventListener("click",()=>{
-    searchinput.value = "";
-    wrapper.classList.remove('active')
-    infotext.innerHTML = `type a word to search`
+closebtn.addEventListener('click', () => {
+    searchinput.value = '';
+    wrapper.classList.remove('active');
+    infoText.innerHTML = 'type a word to search'
 })
